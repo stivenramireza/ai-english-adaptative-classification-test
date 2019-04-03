cf65418a0d3b0c7be1142c2a22dab2ffb60d6e88
camilocruzv <camilocruzv@users.noreply.github.com>
2019-04-03 15:03:37 -0500

    Updating Test, and solving Overfitting

--- DeepLearning/tensorExampleTest.py
+++ DeepLearning/tensorExampleTest.py
@@ -5,28 +5,22 @@ import tensorflow as tf
 import tflearn
 from tflearn.data_utils import load_csv
 
-data, labels = load_csv('testAndGrades2.csv', has_header=True, target_column=0, categorical_labels=True, n_classes=4)
-testX, testY = load_csv('test.csv', has_header=True, target_column=0, categorical_labels=True, n_classes=4)
+data, labels = load_csv('testX.csv', has_header=True, target_column=0, categorical_labels=True, n_classes=6)
+testX, testY = load_csv('testY.csv', has_header=True, target_column=0, categorical_labels=True, n_classes=6)
 
 # Build neural network
 net = tflearn.input_data(shape=[None, 3])
 net = tflearn.fully_connected(net, 32)
 net = tflearn.fully_connected(net, 32)
-net = tflearn.fully_connected(net, 4, activation='sigmoid')
+net = tflearn.fully_connected(net, 6, activation='softplus')
 net = tflearn.regression(net)
 
 # Define model
 model = tflearn.DNN(net)
 # Start training (apply gradient descent algorithm)
-model.fit(data, labels, n_epoch=40, batch_size=1, show_metric=True)
+model.fit(data, labels, n_epoch=100, batch_size=1, show_metric=True)
 
 # Testing model
-student1 = [5.0, 5.0, 5.0]
-student2 = [0.0, 0.0, 0.0]
-student3 = [5.0, 4.0, 3.0]
-
-expected = [3]
-
 predicciones = np.array(model.predict(testX)).argmax(axis=1)
 correctas = testY.argmax(axis=1)
 certeza = np.mean(predicciones == correctas, axis=0)
