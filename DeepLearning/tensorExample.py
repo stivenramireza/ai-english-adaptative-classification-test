0e5ea150dc7fd8f793caf52c68d0ffa9aa36d605
Juan Pablo González <jugonz31@eafit.edu.co>
2019-04-08 23:11:04 -0500

    Actualizaciones en los datos de entrada y pruebas del modelo.
    Co-Authored-By: camilocruzv <camilocruzv@users.noreply.github.com>

--- DeepLearning/tensorExample.py
+++ DeepLearning/tensorExample.py
@@ -6,19 +6,19 @@ from tflearn.data_utils import load_csv
 import csv
 
 data, labels = load_csv('testAndGrades.csv', has_header=True, 
-target_column=0, categorical_labels=True, n_classes=6)
+target_column=0, categorical_labels=True, n_classes=10)
 
 # Construcción de la red neuronal. Definiendo 3 entradas y 6 posibles salidas
 net = tflearn.input_data(shape=[None, 3])
 net = tflearn.fully_connected(net, 32)
 net = tflearn.fully_connected(net, 32)
-net = tflearn.fully_connected(net, 6, activation='softplus')
+net = tflearn.fully_connected(net, 10, activation='softmax')
 net = tflearn.regression(net)
 
 # Definición del modelo
 model = tflearn.DNN(net)
 # Inicio del entrenamiento del modelo
-model.fit(data, labels, n_epoch=100, batch_size=16, show_metric=True)
+model.fit(data, labels, n_epoch=200, batch_size=16, show_metric=True)
 
 #Prueba del modelo
 print("Prueba en tiempo real")
@@ -29,12 +29,16 @@ score = model.predict_label([[calificacionFac,calificacionMed,calificacionDif]])
 array=score[0]
 
 choices = { 
-        0: "El aspirante puede clasificar a los cursos 1, 2 y 3.",
-        1: "El aspirante puede clasificar a los cursos 4, 5 y 6.",
-        2: "El aspirante puede clasificar a los cursos 7, 8 y 9.", 
-        3: "El aspirante puede clasificar a los cursos 10, 11 y 12.", 
-        4: "El aspirante puede clasificar a los cursos 13, 14 y 15.",
-        5: "El aspirante puede clasificar a los cursos 16, 17 y cursos avanzados.",
+        0: "El aspirante puede clasificar al curso de principiantes .",
+        1: "El aspirante puede clasificar a los cursos 1 y 2.",
+        2: "El aspirante puede clasificar a los cursos 3 y 4.", 
+        3: "El aspirante puede clasificar a los cursos 5 y 6.", 
+        4: "El aspirante puede clasificar a los cursos 7 y 8.",
+        5: "El aspirante puede clasificar a los cursos 9 y 10.",
+        6: "El aspirante puede clasificar a los cursos 11 y 12.",
+        7: "El aspirante puede clasificar a los cursos 13 y 14.",
+        8: "El aspirante puede clasificar a los cursos 15 y 16.",
+        9: "El aspirante puede clasificar al curso 17 y a cursos avanzados.",
         }
 print(choices.get(array[0], 'default'))
 print("")
