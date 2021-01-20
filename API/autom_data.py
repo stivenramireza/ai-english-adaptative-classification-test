28a0c0b8ff8c14c2a18992eed5c96a074a882de4
Stiven Ramírez Arango <stivenramireza@gmail.com>
2021-01-19 21:44:35 -0500

    Update autom_data.py

--- API/autom_data.py
+++ API/autom_data.py
@@ -13,28 +13,19 @@ def main():
     """
         Starts the autommation script to extract the data of the database and creates a data file for the model.
     """
-    # FULL CONNECTION STRING TO THE DATABASE
     con_string = MONGO_CONNECTION_URL
-    # MONGO CLIENT
     client = MongoClient(con_string)
-    # DB NAME
     db = client[MONGO_DB_NAME]
-    # QUESTIONS
     cols = db['questions']
-    # CONVERTS TO DATAFRAME
     df = pd.DataFrame(list(cols.find({})))
-    # REMOVES DB ID
     del df['_id']
     del df['updatedAt']
     del df['createdAt']
     del df['__v']
-    # CHANGE COLUMNS NAMES FOR THE MODEL
     df.columns = ['PREGUNTA', 'N_ITEM', 'Parte', 'DIFICULTAD', 'OPCION_CORRECTA', 'TEXTO']
-    # PATH TO SAVE THE QUESTIONS
     path = './data/easy_dataset_12.csv'
     if os.path.exists(path):
         os.remove(path)
-    # SAVES QUESTIONS TO A CSV FILE
     df.to_csv(path)
 
 if __name__ == "__main__":
